7b0bff27fedc943d14f70b864eb0697fe0a4aba6
syre.duc.ng <hoang.d.nguyen@campus.tu-berlin.de>
2022-05-23 03:38:11 +0200

    Add Impressum

---
+++ client/src/components/pages/Impressum/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Impressumseite">
+		<h1 class="text-center">Impressum</h1>
+		<p class="text-center mb-0 mt-0">Michael Herberg</p>
+    <p class="text-center mb-0 mt-0">Daniel Do</p>
+    <p class="text-center mb-0 mt-0">Duc ...</p>
+    <a href="/">Back</a>
+		</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ImpressumPage'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import'~bootstrap/dist/css/bootstrap.css'
+</style>
