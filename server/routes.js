02858ff6c7de3c124014a820ba7650ece6f541c2
michaelherberg <michael@flex.earth>
2022-05-23 07:31:04 +0200

    Added routes to server

---
+++ server/routes.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const routes = express.Router();
+const db = require("./connect");
+
+routes.route("/todos").get((_req, res) => {
+	const connection = db.getDb();
+
+	connection
+		.collection("todos")
+		.find({})
+		.limit(50)
+		.toArray((err, result) => {
+			if (err) {
+				res.status(400).send();
+			}
+			res.json(result);
+		});
+});
+
+routes.route("/todos").post(async (req, res) => {
+	const connection = db.getDb();
+	const todo = {
+		title: req.body.title,
+		date: new Date(req.body.date),
+		percentage: req.body.percentage,
+	};
+
+	try {
+		await connection.collection("todos").insertOne(todo);
+	} catch (err) {
+		res.status(400).send();
+	}
+
+	res.status(204).send();
+});
+
+routes.route("/todos").put(async (req, res) => {
+	const connection = db.getDb();
+	const filter = { _id: new ObjectId(req.body.id) };
+	const update = {
+		$set: {
+			title: req.body.title,
+			date: new Date(req.body.date),
+			percentage: req.body.percentage,
+		},
+	};
+
+	try {
+		await connection.collection("todos").updateOne(filter, update);
+	} catch (err) {
+		res.status(400).send();
+	}
+
+	res.status(204).send();
+});
+
+routes.route("/todos").delete(async (req, res) => {
+	const connection = db.getDb();
+	const filter = { _id: new ObjectId(req.body.id) };
+
+	try {
+		await connection.collection("todos").deleteOne(filter);
+	} catch (err) {
+		res.status(400).send();
+	}
+
+	res.status(204).send();
+});
+
+module.exports = routes;
