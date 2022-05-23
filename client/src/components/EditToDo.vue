146b109fa4448d1ffdfcddf606fda07ad4487d50
daniel-k-d-do <daniel.do2001@gmx.de>
2022-05-23 11:12:50 +0200

    clean up

--- client/src/components/EditToDo.vue
+++ client/src/components/EditToDo.vue
@@ -1,41 +1,41 @@
 <template>
-    <div>
-        <div class="card col">
-            <div class="form-floating mb-3">
-            <input
-                type="text"
-                class="form-control"
-                id="titel"
-                placeholder="Titel des To-Dos"
-                v-model="todo.title"/>
-            <label for="titel">Titel</label>
-            </div>
-            <div class="col-12 d-flex flex-row justify-content-around align-items-center">
-                <div class="form-floating mb-3 col-4">
-                    <input
-                        type="date"
-                        class="form-control"
-                        id="date"
-                        placeholder="Bis wann soll das To-Do bearbeitet werden?"
-                        v-model="date"
-                      />
-                    <label for="date">Datum</label>
-                </div>
-                <div class="form-floating mb-3 col-4">
-                   <input
-                        type="number"
-                        class="form-control"
-                        id="percentage"
-                        placeholder="Wie viel des To-Dos ist bereits geschafft?"
-                        v-model="todo.percentage"/>
-                    <label for="percentage">Prozent</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <button class="btn btn-primary" @click="submit">Speichern</button>
-                </div>
-            </div>
-        </div> 
-    </div>      
+  <div>
+    <div class="card col">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="titel"
+          placeholder="Titel des To-Dos"
+          v-model="todo.title"/>
+        <label for="titel">Titel</label>
+      </div>
+      <div class="col-12 d-flex flex-row justify-content-around align-items-center">
+        <div class="form-floating mb-3 col-4">
+          <input
+            type="date"
+            class="form-control"
+            id="date"
+            placeholder="Bis wann soll das To-Do bearbeitet werden?"
+            v-model="date"
+          />
+          <label for="date">Datum</label>
+        </div>
+        <div class="form-floating mb-3 col-4">
+          <input
+            type="number"
+            class="form-control"
+            id="percentage"
+            placeholder="Wie viel des To-Dos ist bereits geschafft?"
+            v-model="todo.percentage"/>
+          <label for="percentage">Prozent</label>
+        </div>
+        <div class="form-floating mb-3">
+          <button class="btn btn-primary" @click="submit">Speichern</button>
+        </div>
+      </div>
+    </div> 
+  </div>      
 </template>
 
 <script>
@@ -74,7 +74,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import'~bootstrap/dist/css/bootstrap.css'
 </style>
